26ed5549708226470ac22c29ab1476391a8f2d89
lijiacong-coder <1143596375@qq.com>
2020-03-07 17:30:46 +0800

    vue-cli

---
+++ test/src/views/Mine.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        Mine component.
+        <!-- {{$route.params.id}}
+        {{$route.params.name}} -->
+
+        {{$route.query.id}}
+        {{$route.query.name}}
+        <router-link to="/mine/test1">子路由1 |</router-link>
+        <router-link to="/mine/test2">子路由2</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                msg:'haha'
+            }
+        },
+        methods:{
+
+        },
+        created(){
+            console.log(this.msg);
+            console.log(this.$route.params.id);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ test/src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Test page.
+
+        {{
+            $route.params.id
+        }}
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
